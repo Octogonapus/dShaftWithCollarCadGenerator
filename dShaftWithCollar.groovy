a9be8057eba5f835519618ea450100d34e8958d6
Octogonapus <firey45@gmail.com>
2020-01-22 13:53:57 -0500

    new CAD loader script

---
+++ dShaftWithCollar.groovy
@@ -0,0 +1,38 @@
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG generate(){
+	String type= "dShaftWithCollar"
+	if(args==null)
+		args=["5mm_dShaft"]
+	// The variable that stores the current size of this vitamin
+	StringParameter size = new StringParameter(	type+" Default",args.get(0),Vitamins.listVitaminSizes(type))
+	HashMap<String,Object> measurments = Vitamins.getConfiguration( type,size.getStrValue())
+
+	def collarLengthValue = measurments.collarLength
+	def shaftDSectionDiameterValue = measurments.shaftDSectionDiameter
+	def priceValue = measurments.price
+	def massCentroidYValue = measurments.massCentroidY
+	def massCentroidXValue = measurments.massCentroidX
+	def collarDiameterValue = measurments.collarDiameter
+	def sourceValue = measurments.source
+	def massCentroidZValue = measurments.massCentroidZ
+	def massKgValue = measurments.massKg
+	def shaftLengthValue = measurments.shaftLength
+	def shaftDiameterValue = measurments.shaftDiameter
+	println "Measurment collarLengthValue =  "+collarLengthValue
+	println "Measurment shaftDSectionDiameterValue =  "+shaftDSectionDiameterValue
+	println "Measurment priceValue =  "+priceValue
+	println "Measurment massCentroidYValue =  "+massCentroidYValue
+	println "Measurment massCentroidXValue =  "+massCentroidXValue
+	println "Measurment collarDiameterValue =  "+collarDiameterValue
+	println "Measurment sourceValue =  "+sourceValue
+	println "Measurment massCentroidZValue =  "+massCentroidZValue
+	println "Measurment massKgValue =  "+massKgValue
+	println "Measurment shaftLengthValue =  "+shaftLengthValue
+	println "Measurment shaftDiameterValue =  "+shaftDiameterValue
+	// Stub of a CAD object
+	CSG part = new Cube().toCSG()
+	return part
+		.setParameter(size)
+		.setRegenerate({generate()})
+}
+return generate() 
